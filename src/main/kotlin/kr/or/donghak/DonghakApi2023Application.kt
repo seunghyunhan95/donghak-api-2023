4d7087e9ba96b70a8f94f5ebdce5bb24c2d5d3a8
seunghyun <sh.han@sqisoft.com>
2023-11-01 16:52:14 +0900

    Initial commit

---
+++ src/main/kotlin/kr/or/donghak/DonghakApi2023Application.kt
@@ -0,0 +1,11 @@
+package kr.or.donghak
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DonghakApi2023Application
+
+fun main(args: Array<String>) {
+    runApplication<DonghakApi2023Application>(*args)
+}
---
+++ src/test/kotlin/kr/or/donghak/DonghakApi2023ApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.or.donghak
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DonghakApi2023ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
